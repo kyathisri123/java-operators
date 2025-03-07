a52e7cf4bb911642d3bbb4617c9633941f370cad
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:13:23 +0530

    Add files via upload

---
+++ EqualNotEqual.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class EqualNotEqual {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+    }
+}
+
