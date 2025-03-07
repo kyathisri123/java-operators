c1072aad558b3992f366810486f00fd7c7651a9d
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:14:18 +0530

    Add files via upload

---
+++ MinMaxNumber.java
@@ -0,0 +1,21 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class MinMaxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        int min = (num1 < num2) ? num1 : num2;
+        int max = (num1 > num2) ? num1 : num2;
+        
+        System.out.println("Smaller number: " + min);
+        System.out.println("Larger number: " + max);
+        
+        scanner.close();
+    }
+}
